a21ec0f9556ca0725966d8bacf5a96b86df16be1
Duc Le <duclm1992@gmail.com>
2017-01-13 11:05:33 +0700

    Finish Chapter 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_id params[:id]
     if @user.nil?
-      flash[:waring] = "User not found!"
+      flash[:danger] = "User not found!"
       redirect_to signup_path
     end
   end
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else

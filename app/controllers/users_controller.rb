824831efcbf6dcd8cefb5c45dc61d09b04854bb1
Duc Le <duclm1992@gmail.com>
2017-01-16 20:10:06 +0700

    Finish Updating, showing, and deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find_by_id params[:id]
-    if @user.nil?
-      flash[:danger] = "User not found!"
-      redirect_to signup_path
-    end
+  before_action :logged_in_user, except: [:new, :create]
+  before_action :find_user, except: [:index, :new, :create]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate page: params[:page]
   end
 
   def new
@@ -22,9 +23,54 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = "Profile updated!"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = "User deleted!"
+    redirect_to users_url
+  end
+
   private
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    if @user.nil?
+      flash[:danger] = "User not found!"
+      redirect_to root_url
+    end
+  end
+
+  def correct_user
+    redirect_to root_url unless current_user.current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
   end
 end

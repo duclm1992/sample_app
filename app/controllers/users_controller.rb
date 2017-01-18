84142c046974d2bcabd7cb56b4a96557ca9f34aa
Duc Le <duclm1992@gmail.com>
2017-01-18 08:28:46 +0700

    Finish Activation account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, except: [:new, :create]
+  before_action :admin_user, only: :destroy
   before_action :find_user, except: [:index, :new, :create]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: :destroy
 
   def index
-    @users = User.paginate page: params[:page]
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def new
@@ -15,15 +15,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_path
     else
-      render "new"
+      render :new
     end
   end
 
   def show
+    @user = User.find_by id: params[:id]
+    redirect_to root_path and return false unless @user.activated?
   end
 
   def edit
@@ -34,7 +36,7 @@ class UsersController < ApplicationController
       flash[:success] = "Profile updated!"
       redirect_to @user
     else
-      render "edit"
+      render :edit
     end
   end
 
@@ -60,17 +62,17 @@ class UsersController < ApplicationController
 
   def find_user
     @user = User.find_by id: params[:id]
-    if @user.nil?
+    unless @user
       flash[:danger] = "User not found!"
-      redirect_to root_url
+      redirect_to root_path
     end
   end
 
   def correct_user
-    redirect_to root_url unless current_user.current_user? @user
+    redirect_to root_path unless current_user.current_user? @user
   end
 
   def admin_user
-    redirect_to root_url unless current_user.admin?
+    redirect_to root_path unless current_user.admin?
   end
 end

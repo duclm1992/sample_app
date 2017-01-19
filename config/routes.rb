c3bb75f3eb5172edf7a09b159c05c708fe40c4b9
Duc Le <duclm1992@gmail.com>
2017-01-19 19:41:02 +0700

    Finish Password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root "static_pages#home"
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
@@ -9,4 +10,5 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#destroy"
   resources :users, except: [:new]
   resources :account_activations, only: [:edit]
+  resources :password_resets, except: [:index, :show, :destroy]
 end
